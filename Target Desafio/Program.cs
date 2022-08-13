d9e6e9194c870475d3e29e33c79d3813ade67de3
Alexsandro-Santos-Junior <alexsandro_azuras@hotmail.com>
2022-08-13 11:05:27 -0300

    Desafio Target

---
+++ Target Desafio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13;
+            int Soma = 0;
+            int K = 0;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+                Soma = Soma + K;
+
+
+                     
+            }
+            Console.WriteLine(Soma);
+            Console.ReadLine();
+        }
+        
+    }
+}
